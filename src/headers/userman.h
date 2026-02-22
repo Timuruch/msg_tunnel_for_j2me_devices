3dc499c8b5e67c2cc2445863f5389e8d9734adea
timuruch <timuruch909@gmail.com>
2026-02-22 19:27:34 +0100

    Defining userman, to continue work tmr.

---
+++ src/headers/userman.h
@@ -0,0 +1,19 @@
+/*
+ * 	Project: msg_tunnel_for_old_j2me_devices
+ * 	File: src/headers/userman.h (User Manager)
+ * 	By: Timuruch (timuruch909@gmail.com)
+ *
+ * 	Note: I know that saving users in users.us
+ * 	and passwords.ps in raw format is really 
+ * 	stupid and insecure... BUT! Since this file
+ * 	would be only seen by host (if he would
+ * 	explicitly open it) and not by some weirdos
+ * 	from the internet (if ofc the server would
+ * 	not be hacked by these weridos). So in theory
+ * 	the whole data story should be alr.
+*/
+#include <stdio.h>
+
+void checkPass(); //check_password
+
+void regNewClient(); //register new client to the database
